af497e714893638c9bd20074f6b4ea747b9ca78c
Harol Mauricio Gómez Zapata <harolmgzapata@gmail.com>
2025-11-05 20:32:52 -0500

    cambio de ruta

--- apprender/urls.py
+++ apprender/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('insertar/usuario/', views.insertar, name="insertar_u"),
+    path('insertar_usuario/', views.insertar, name="insertar_u"),
 ]
