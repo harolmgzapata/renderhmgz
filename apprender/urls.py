dea645c17e1e958fe1c22d52c4aa7a51f6df0fce
Harol Mauricio Gómez Zapata <harolmgzapata@gmail.com>
2025-11-05 20:02:15 -0500

    agregando primera ruta y archivo crud.html

--- apprender/urls.py
+++ apprender/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('insertar/usuario/', views.insertar, name="insertar_u"),
 ]
--- apprender/views.py
+++ apprender/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Usuarios
 
 # Create your views here.
 
 def index(request):
     return render(request, "apprender/index.html")
+
+
+def insertar(request):
+    if request.POST:
+        correo = request.POST['correo']
+        clave = request.POST['clave']
+        usuario = Usuarios()
+        usuario.correo = correo
+        usuario.clave = clave
+        usuario.save()
+        usuarios = Usuarios.objects.all()
+        contexto = {'usuarios': usuarios}
+        return render(request, 'apprender/crud.html', contexto)
